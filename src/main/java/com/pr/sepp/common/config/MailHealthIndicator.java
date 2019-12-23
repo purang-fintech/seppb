4702fa4c7f010c336c51b83b92fecc8ca29d21df
liuy <yijiubasi@163.com>
2019-12-23 17:52:16 +0800

    邮件健康检查问题解决

---
+++ src/main/java/com/pr/sepp/common/config/MailHealthIndicator.java
@@ -0,0 +1,21 @@
+package com.pr.sepp.common.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailHealthIndicator implements HealthIndicator {
+	@Override
+	public Health health() {
+		int errorCode = check();
+		if (errorCode != 0) {
+			return Health.down().withDetail("Error Code", errorCode).build();
+		}
+		return Health.up().build();
+	}
+
+	int check() {
+		return 0;
+	}
+}
